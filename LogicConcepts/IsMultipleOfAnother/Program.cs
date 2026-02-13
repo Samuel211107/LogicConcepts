2eb860e4c8c5826b8b26743b25e3954b9881e64a
Samuel211107 <146089377+Samuel211107@users.noreply.github.com>
2026-02-12 23:24:42 -0500

    Complete exercice 04

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese el primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo número: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo {b}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Quieres continuar? [S]í, [N]o: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("¡Hasta luego!");
+
