5d73250aef6b2fe9e18b437e86b651499a025303
Samuel211107 <146089377+Samuel211107@users.noreply.github.com>
2026-02-19 05:05:04 -0500

    Complete exercice 19

--- LogicConcepts/Shared/MyMath.cs
+++ LogicConcepts/Shared/MyMath.cs
@@ -9,11 +9,24 @@ namespace Shared
         public static double Factorial(int n)
         {
             double factorial = 1;
-            for   (int i = 2; i <= n; i++)
+            for (int i = 2; i <= n; i++)
             {
                 factorial *= i;
             }
             return factorial;
         }
-    }
+
+
+       public static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+
+            }
+            return true;
+        }
+
+
+    }   
 }
